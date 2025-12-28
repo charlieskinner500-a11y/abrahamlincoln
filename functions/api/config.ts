3a58d7b780b34d873bccf6d802bf7071a963ac6f
Charlie Skinner <charlieskinner@mac.lan>
2025-12-29 03:54:30 +1100

    make load public

--- functions/api/config.ts
+++ functions/api/config.ts
@@ -1,7 +1,4 @@
 export async function onRequestGet({ request, env }) {
-  if (request.headers.get("x-admin-token") !== env.ADMIN_TOKEN) {
-    return new Response("Unauthorized", { status: 401 });
-  }
 
   const url = new URL(request.url);
   const key = url.searchParams.get("key") || "config";
